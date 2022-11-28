5cdc388bd0bf41b9a09d74a952d09ee4d6cfa623
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-11-27 22:23:50 -0600

    created workout service test method signatures

---
+++ gainz-microservices/workout-service/src/main/java/com/miguelbarrios/gainz/workoutservice/services/WorkoutServiceImpl.java
@@ -0,0 +1,30 @@
+package com.miguelbarrios.gainz.workoutservice.services;
+
+import com.miguelbarrios.gainz.workoutservice.models.Workout;
+
+public class WorkoutServiceImpl implements WorkoutService{
+    @Override
+    public void createWorkout(int userId, Workout workout) {
+
+    }
+
+    @Override
+    public void getWorkout(int userId, int workoutId) {
+
+    }
+
+    @Override
+    public void updateWorkout(int userId, Workout workout) {
+
+    }
+
+    @Override
+    public void deleteWorkout(int userId, int workoutId) {
+
+    }
+
+    @Override
+    public void getUserWorkouts(int userId) {
+
+    }
+}
---
+++ gainz-microservices/workout-service/src/test/java/com/miguelbarrios/gainz/workoutservice/services/WorkoutServiceTest.java
@@ -0,0 +1,47 @@
+package com.miguelbarrios.gainz.workoutservice.services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WorkoutServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+
+    @Test
+    void createWorkout() {
+        assertTrue(false);
+    }
+
+    @Test
+    void getWorkout() {
+        assertTrue(false);
+    }
+
+    @Test
+    void updateWorkout() {
+        assertTrue(false);
+
+    }
+
+    @Test
+    void deleteWorkout() {
+        assertTrue(false);
+
+    }
+
+    @Test
+    void getUserWorkouts() {
+        assertTrue(false);
+
+    }
+}
