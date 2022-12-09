639cde187ad6931321c10a84d30a80daf7b8c093
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-12-08 23:41:50 -0600

    created User and Role class

--- gainz-microservices/user-service/src/main/java/com/miguelbarrios/userservice/UserServiceApplication.java
+++ gainz-microservices/user-service/src/main/java/com/miguelbarrios/userservice/UserServiceApplication.java
@@ -2,6 +2,9 @@ package com.miguelbarrios.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class UserServiceApplication {
@@ -9,5 +12,8 @@ public class UserServiceApplication {
         SpringApplication.run(UserServiceApplication.class, args);
     }
 
-
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
