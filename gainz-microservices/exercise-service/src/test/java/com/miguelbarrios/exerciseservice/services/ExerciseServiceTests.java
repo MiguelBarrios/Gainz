26d134e232ead0ca91c68317345efd3bcde8d36a
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-12-19 20:31:53 -0600

    refactored exercise service tests

--- gainz-microservices/exercise-service/src/test/java/com/miguelbarrios/exerciseservice/services/ExerciseServiceTests.java
+++ gainz-microservices/exercise-service/src/test/java/com/miguelbarrios/exerciseservice/services/ExerciseServiceTests.java
@@ -13,22 +13,12 @@ import java.util.Set;
 import com.miguelbarrios.exerciseservice.exceptions.ExerciseNotFoundException;
 import com.miguelbarrios.exerciseservice.models.Exercise;
 import com.miguelbarrios.exerciseservice.repositories.ExerciseRepository;
-
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith( SpringRunner.class )
 @SpringBootTest
-@ExtendWith(SpringExtension.class)
 class ExerciseServiceTests {
 	
 	@Autowired
@@ -38,30 +28,31 @@ class ExerciseServiceTests {
 	private ExerciseServiceImpl exerciseService;
 	
 	private static int userId = 1;
-	
 	private static int userId2 = 2;
-	
-
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-
-	}
-
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		
+		resetDB();
 	}
-
-	@AfterEach
-	void tearDown() throws Exception {
+	
+	void resetDB() {
+		exerciseRepository.deleteAll();
+		List<Exercise> exercises = new ArrayList<>();
+		exercises.add(new Exercise("Bench Press", false, null, null));
+		exercises.add(new Exercise("Shoulder Press", false, null, null));
+		exercises.add(new Exercise("Lat Pull", false, null, null));
+		exercises.add(new Exercise("Squat", true, userId, null));
+		exercises.add(new Exercise("DeadLift", true, userId, null));
+		exercises.add(new Exercise("Power Clean", true, userId, null));
+		exercises.add(new Exercise("Leg Press", true, userId2, null));
+		exercises.add(new Exercise("Curls", true, userId2, null));
+	
+		exerciseRepository.saveAll(exercises);
 	}
 	
 	@Test
 	void should_get_exercise_by_id() {
-		Set<String> targetedMuscles = new HashSet<>();
 		Exercise exercise = Exercise.builder()
 				.name("Bench Press")
 				.build();
@@ -87,11 +78,7 @@ class ExerciseServiceTests {
 	@Test
 	void should_get_all_exercises() {
 		List<Exercise> exercises = new ArrayList<>();
-		exercises.add(new Exercise("Bench Press", false, null, null));
-		exercises.add(new Exercise("Shoulder Press", false, null, null));
-		exercises.add(new Exercise("Lat Pull", false, null, null));
-		exercises.add(new Exercise("Squat", false, userId, null));
-		exercises.add(new Exercise("DeadLift", false, userId, null));
+		exerciseRepository.saveAll(exercises);
 		
 		List<Exercise> actual = exerciseService.getExercise();
 		assertNotNull(actual);
@@ -100,14 +87,6 @@ class ExerciseServiceTests {
 	
 	@Test
 	void should_get_user_created_exercises() {
-		List<Exercise> exercises = new ArrayList<>();
-		exercises.add(new Exercise("Bench Press", true, userId, null));
-		exercises.add(new Exercise("Shoulder Press", false, null, null));
-		exercises.add(new Exercise("Lat Pull", false, null, null));
-		exercises.add(new Exercise("Squat", true, userId2, null));
-		exercises.add(new Exercise("DeadLift", true, userId2, null));
-		exerciseRepository.saveAllAndFlush(exercises);
-		
 		List<Exercise> userExercises = exerciseService.getExercisesCreatedbyUser(userId2);
 		assertNotNull(userExercises);
 		assertEquals(2, userExercises.size());
@@ -149,16 +128,7 @@ class ExerciseServiceTests {
 	}
 	
 	@Test
-	void should_delete_all_user_created_exercises() {
-		exerciseRepository.deleteAll();
-		
-		int userId = 4;
-		List<Exercise> exercises = new ArrayList<>();
-		exercises.add(new Exercise("Bench Press", true, userId, null));
-		exercises.add(new Exercise("Squat", true, userId, null));
-		exercises.add(new Exercise("DeadLift", true, userId, null));
-		exerciseRepository.saveAllAndFlush(exercises);
-		
+	void should_delete_all_user_created_exercises() {		
 		exerciseService.removeAllCustomExercisesCreatedByUser(userId);
 		List<Exercise> userExercises = exerciseRepository.findAllByUserId(userId); 
 		assertNotNull(userExercises);
@@ -167,37 +137,20 @@ class ExerciseServiceTests {
 	
 	@Test
 	void should_delete_exercise_by_id() {
-		exerciseRepository.deleteAll();
-		List<Exercise> exercises = new ArrayList<>();
-		exercises.add(new Exercise("Bench Press", true, userId, null));
-		exercises.add(new Exercise("Squat", true, userId, null));
-		exercises.add(new Exercise("DeadLift", true, userId, null));
-		exerciseRepository.saveAllAndFlush(exercises);
-		
 		List<Exercise> userExercises = exerciseRepository.findAllByUserId(userId);
 		int exerciseId = userExercises.get(0).getId();
 		
 		boolean deleted = exerciseService.deleteExercise(exerciseId, userId);
 		assertTrue(deleted);
 		userExercises = exerciseRepository.findAllByUserId(userId);
-		
 		assertEquals(2, userExercises.size());
-
 	}
 	
 	@Test
-	void should_get_all_user_created_exercises() {
-		exerciseRepository.deleteAll();
-		List<Exercise> exercises = new ArrayList<>();
-		exercises.add(new Exercise("Bench Press", true, 1, null));
-		exercises.add(new Exercise("Squat", true, 1, null));
-		exercises.add(new Exercise("DeadLift", true, 2, null));
-		exercises.add(new Exercise("DeadLift 2", false, null, null));
-		exerciseRepository.saveAllAndFlush(exercises);
-		
+	void should_get_all_user_created_exercises() {		
 		List<Exercise> userCreatedExercises =  exerciseService.getAllCustomExercises();
 		assertNotNull(userCreatedExercises);
-		assertEquals(3, userCreatedExercises.size());
+		assertEquals(5, userCreatedExercises.size());
 	}
 	
 	
